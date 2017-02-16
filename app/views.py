8bf04f2a1b832328ba326dac374c62fb8eddcc8e
Marius Aarsnes <mariaars@stud.ntnu.no>
2017-02-16 20:15:23 +0100

    fjerner template greier for å se om det funker uten

--- app/views.py
+++ app/views.py
@@ -1,10 +1,14 @@
-from flask import render_template
-from app import app
-
 @app.route('/')
 @app.route('/index')
 def index():
     user = {'nickname': 'Miguel'}  # fake user
-    return render_template('index.html',
-                           title='Home',
-                           user=user)
+    return '''
+<html>
+  <head>
+    <title>Home Page</title>
+  </head>
+  <body>
+    <h1>Hello, ''' + user['nickname'] + '''</h1>
+  </body>
+</html>
+'''
