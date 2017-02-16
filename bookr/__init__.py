43fcf632c89dd9c39fcfe745e69937dd96564deb
Marius Aarsnes <mariaars@stud.ntnu.no>
2017-02-16 20:01:33 +0100

    fjerner alt søppelet

--- bookr/__init__.py
+++
@@ -1,4 +0,0 @@
-import bookr.forms
-import bookr.login
-import bookr.validation
-import bookr.views
--- config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-from bookr.app import app
-
-DEBUG = True
-SECRET_KEY = 'dev'
-DATABASE = os.path.join(app.root_path, 'main.db')
