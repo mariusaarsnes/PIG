919871d08eb04d1866fa677c2577794b92fbf818
Marius Aarsnes <mariaars@stud.ntnu.no>
2017-02-17 16:09:45 +0100

    fjernet kode sånn at index skal fungere igjen

--- pig/app.py
+++ pig/app.py
@@ -9,18 +9,5 @@ from flask import url_for
 app = Flask(__name__, template_folder='templates')
 
 @app.route("/")
-@app.route("/index")
 def index():
     return render_template('index.html')
-
-@app.route("/login")
-def login():
-    return render_template("login.html")
-
-@app.route("apply_group")
-def apply_group():
-    return render_template("apply_group.html")
-
-@app.route("create_division")
-def create_division():
-    return render_template("create_division.html")
