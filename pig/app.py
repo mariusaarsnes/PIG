b7ea24f0bd160697c51fb0cecac8592d7dd7a875
Sebastian Olafsson <msemse.sebastian@gmail.com>
2017-02-17 02:25:57 +0100

    this makes the index work

---
+++ pig/app.py
@@ -0,0 +1,15 @@
+import os
+from flask import Flask
+from flask import redirect
+from flask import render_template
+from flask import request
+from flask import session
+from flask import url_for
+
+app = Flask(__name__, template_folder='templates')
+
+@app.route("/")
+def hello():
+    return render_template('index.html')
+
+
