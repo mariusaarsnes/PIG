8ecaee02765dbfeb14aa2660d02a9503e181daf2
Marius Aarsnes <mariaars@stud.ntnu.no>
2017-02-22 20:36:00 +0100

    bugfix

--- pig/app.py
+++ pig/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 db = SQLAlchemy(app)
 
