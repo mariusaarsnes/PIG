ec67868a346c3db2f41f2b10d5450771d357bc9b
Marius Aarsnes <mariaars@stud.ntnu.no>
2017-02-22 17:35:08 +0100

    satt opp en enkel database på heroku og lagt til code for å koble til den

--- pig/app.py
+++ pig/app.py
@@ -4,15 +4,10 @@ from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__, template_folder='templates')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+db = SQLAlchemy(app)
 
-mysql = SQLAlchemy(app)
- 
-# MySQL configurations
-app.config['MYSQL_DATABASE_USER'] = 'mariaars_pu'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'pu'
-app.config['MYSQL_DATABASE_DB'] = 'sebasto_pig'
-app.config['MYSQL_DATABASE_HOST'] = 'mysql.stud.ntnu.no'
-mysql.init_app(app)
+all_users =db.query_all();
 
 @app.route("/")
 def hello():
