7638de0bc002039141a0c52ab499a06680ce4306
Marius Aarsnes <mariaars@stud.ntnu.no>
2017-02-22 19:35:53 +0100

    kommenterer ut feil kode

--- pig/app.py
+++ pig/app.py
@@ -1,13 +1,9 @@
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
-import os
-import models
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+#app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 db = SQLAlchemy(app)
-
-
 
 @app.route("/")
 def hello():
