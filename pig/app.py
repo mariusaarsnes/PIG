d88837ea2956677d924d78e9f18de6b32606456a
Marius Aarsnes <mariaars@stud.ntnu.no>
2017-02-22 20:28:50 +0100

    whatever

--- pig/app.py
+++ pig/app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-#app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 db = SQLAlchemy(app)
 
 @app.route("/")
