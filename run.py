87f387ce0262c895c3289f5c8c976599f59fd378
Marius Aarsnes <mariaars@stud.ntnu.no>
2017-02-27 18:15:22 +0100

    fikset merge conflict i run.py

--- run.py
+++ run.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import os
 
 
@@ -10,15 +9,3 @@ from pig.views import app
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
-
-=======
-
-import os
-
-from pig.views import app
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
-
->>>>>>> 08d432c08911485d8a3233131a3adf6898b14f2b
