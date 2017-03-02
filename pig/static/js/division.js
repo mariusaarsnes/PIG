87a4f6f89660deb8148d55b7dc9a2d0066ebf6e2
Erlend Hofstad Langseth <3rlendhl@gmail.com>
2017-03-02 21:05:13 +0100

    WIP -  unintentional: page refreshes when pressing '+' button.

--- pig/static/js/division.js
+++ pig/static/js/division.js
@@ -2,13 +2,14 @@ var paraId = 0;
 
 document.getElementById("add_button").addEventListener("click", function() {
     var parameterForm = document.createElement("div");
+    parameterForm.style.borderStyle = "solid";
     document.getElementById("division-form").insertBefore(parameterForm, document.getElementById("add_button"));
 
     // Display a form to add a parameter to a division.
     console.log(document.getElementById("division-form").childElementCount);
 
     var label = document.createElement("label");
-    label.innerHTML = "Legg til parameter:";
+    label.innerHTML = "Parameter:";
 
     var nameField = document.createElement("input");
     nameField.setAttribute("type", "text");
@@ -18,19 +19,19 @@ document.getElementById("add_button").addEventListener("click", function() {
     nameField.setAttribute("placeholder", "Parameternavn");
 
 
-    var cross = document.createElement("img");
-   
-    cross.setAttribute("id", "remove_button");
+    var deleteBtn = document.createElement("button");
+    deleteBtn.innerHTML = "Delete";
 
-    parameterForm.appendChild(document.createElement("br"));
     parameterForm.appendChild(label);
-    parameterForm.appendChild(document.createElement("br"));
     parameterForm.appendChild(nameField);
+    parameterForm.appendChild(deleteBtn);
+    parameterForm.appendChild(document.createElement("br"));
     parameterForm.appendChild(typeSelection());
     parameterForm.appendChild(numberForm());
-    parameterForm.appendChild(cross);
+    parameterForm.appendChild(document.createElement("br"));
+    parameterForm.appendChild(enumForm());
 
-    cross.addEventListener("click", function() {
+    deleteBtn.addEventListener("click", function() {
         document.getElementById("division-form").removeChild(parameterForm);
     });
 });
@@ -39,7 +40,10 @@ function typeSelection() {
     var select = document.createElement("select");
     var numOption = document.createElement("option");
     numOption.text = "Number";
+    var enumOption = document.createElement("option");
+    enumOption.text = "Options";
     select.options.add(numOption);
+    select.options.add(enumOption);
     return select;
 }
 
@@ -53,12 +57,46 @@ function numberForm() {
     // Container so that we may change visibility of both fields
     var container = document.createElement("span");
     container.appendChild(min);
+    container.innerHTML += " - ";
     container.appendChild(max);
     container.style.visibility = "visible";
 
     return container;
 }
 
+// TODO names?
 function enumForm() {
-    
+    var addButton = document.createElement("button");
+    addButton.id = "add_variant_button";
+    addButton.innerHTML = "+"
+
+    var container = document.createElement("span");
+    container.appendChild(addButton);
+
+    addButton.addEventListener("click", function() {
+        // Add another variant form
+        addVariantFormTo(container);
+    });
+
+    return container;
 }
+
+function addVariantFormTo(container) {
+    var variantForm = document.createElement("div");
+    var textField = document.createElement("input");
+    var deleteButton = document.createElement("button");
+
+    variantForm.appendChild(textField);
+    variantForm.appendChild(deleteButton);
+
+
+    var addButton = container.querySelector("#add_variant_button");
+    container.insertBefore(variantForm, addButton);
+
+    deleteButton.addEventListener("click", function() {
+        container.removeChild(variantForm);
+    });
+}
+
+
+// TODO legg inn div med fixed width før enum selection? Eller samme width som typeSelection?
