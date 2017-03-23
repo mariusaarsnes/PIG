d735b1401739dde5bcd1eec7019efc8ed55ac74e
Marius Aarsnes <mariaars@stud.ntnu.no>
2017-03-23 18:06:18 +0100

    endret på database slik at den tar inn ett valfritt argument uri for å kunne kjøre tester mot en annen database enn den som brukes av systemet

--- pig/db/database.py
+++ pig/db/database.py
@@ -5,9 +5,9 @@ from flask_sqlalchemy import SQLAlchemy
 #Class that handles the database connection
 class Database:
 
-    def __init__(self, app):
+    def __init__(self, app, uri="postgres://wtsceqpjdsbhxw:34df69f4132d39ea5b95e52822d6dedc8e3eb368915cb8888526f896f21bce75@ec2-54-75-229-201.eu-west-1.compute.amazonaws.com:5432/dfa7tvu04d7t6i"):
         self.app = app
-        self.app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://wtsceqpjdsbhxw:34df69f4132d39ea5b95e52822d6dedc8e3eb368915cb8888526f896f21bce75@ec2-54-75-229-201.eu-west-1.compute.amazonaws.com:5432/dfa7tvu04d7t6i"
+        self.app.config['SQLALCHEMY_DATABASE_URI'] = uri
         self.app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
         self.db = SQLAlchemy(self.app)
 
